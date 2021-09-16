dc3875b8d16fce7f19f37207b3d1190ff89251d1
dimonf <camel109@gmail.com>
2021-09-16 21:14:13 +0300

    backup functions/driver are OK
      except `docker run` - invocation of this ruins following
      ssh connections even if pause of 3-5 sec applied via `sleep`.

--- troj-bu.sh
+++ troj-bu.sh
@@ -4,40 +4,58 @@
 
 #ARGS:
 # - config file. backup-troj.conf by default
+# - mode: backup|bu - backup mode (default
+#         restore - restore mode
 
+bu_mode="bu"
+while [[ $# -gt 0 ]]; do
+				key="$1"
+				case "$key" in
+    				--restore)
+							bu_mode="restore"
+							shift ;;
+	      		        *)
+							config_file="$key"
+							shift ;;
+        esac
+done
 
 #assumptions:
 # - mysql container: tar is installed
 # - on local PC installed: jq
 # - conf file structure (json):
-#   {
-#     "conf": {
-#         "bu_dir":"</path/to/bu/dir>
+#{
+#  "conf": {
+#    "bu_dir": "~/usb-hd/plain/bu"
+#  },
+#  "containers": {
+#    "piwigo": {
+#      "conf": {
+#        "bu_dir": "~/tmp"
+#      },
+#      "tasks": [
+#        {
+#          "driver": "mysqldump",
+#          "container": "sql-server",
+#          "db": "piwigo-db",
+#          "username": "piwigo-username",
+#          "password": "piwigo-password"
+#        },
+#        {
+#          "driver": "rsync",
+#          "src": [
+#            "/gallery",
+#            "/config"
+#          ]
+#        }
+#      ]
+#    },
+#    "another_cont" :{
+#        ...
 #      }
-#     "containers":  [ 
-#            {"container": <container name>,
-#            "bu_dir": </path/to/bu/dir> #overrides bu_dir of global level
-#            "rsync":{
-#                   "src": [
-#                          "/path/to/files",
-#                    ]
-#             }
-#            "mysqldump":{
-#                   "src": [
-#                          {
-#                              "container": <DB container name>,
-#                              "db":        <DB name>,
-#                              "username":  <username>,
-#                             "password":  <password>
-#                          }
-#                    ]
-#                }
-#            },
-#            {....}
-#   ]
+#  }
 #}
-
-CONF_FL=${1:-backup-troj.conf}
+CONF_FL=${config_file:-backup-troj.conf}
 SSH_LOCAL_CONF="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
 REMOTE_HOST=$(docker context inspect | jq '.[].Endpoints.docker.Host' \
@@ -73,7 +91,8 @@ function bu-local-dir {
      # ARGS:
 		 # 1. name of "master" container for which backup is made
 		 # 2. JSON object with parameters
-      bu_dir="$(echo "$2" | jq -er .bu_dir?)" || bu_dir="$BU_DIR"
+		  bu_dir="$(jq -er ".containers[\"$1\"].conf?.bu_dir?"  $CONF_FL)" || bu_dir=$BU_DIR
+#      bu_dir="$(echo "$2" | jq -er .bu_dir?)" || bu_dir="$BU_DIR"
       bu_dir="${bu_dir/#\~/$HOME}"/${1}
 			[[ -d "$bu_dir" ]] || { mkdir -p "$bu_dir"; }
 			echo "$bu_dir"
@@ -105,13 +124,14 @@ function remove_bu_container {
 		fi
 }
 
+
 function check_container_is_on {
 		docker ps -a --format "{{json .}}"| jq -r ".|select(.Names == \"$CONT_BU\").Status?" \
 						|grep -iq '^Up'
 		ssh 
 }
 ########################################################################################
-function sql-bu {
+function mysqldump-bu {
              # ARGS:
 						 # 1. name of "master" container for which backup is done
 				     # 2. JSON object with parameters
@@ -130,13 +150,16 @@ function sql-bu {
           && log 5 "local file recorded:" && log 0 "$(ls -lh $arch_file)"
 }
 
+function mysqldump-restore {
+      echo "Bo"
 
+}
 function rsync-bu {
         # ARGS:
 				# 1. name of "master" container for which backup is done
 				# 2. JSON object with parameters
         create_bu_image
-				remove_bu_container
+				#remove_bu_container
 
 				bu_cont="$1"
 				src_dirs="$(echo "$2" | jq -r .src[])"
@@ -149,56 +172,77 @@ function rsync-bu {
 					#echo "$IMAGE_BU"
 					#echo "$src_dirs"
 
+docker_cmd=$(tr '\n' ' ' <<DOCKER_RUN_END
+docker run -d 
+--name=$CONT_BU 
+$src_dirs 
+-e PUID=1001 
+-e PGID=1001 
+-e PASSWORD_ACCESS=true 
+-e USER_PASSWORD=boo 
+-e USER_NAME=${bu_username}
+-e PUBLIC_KEY="$(cat ~/.ssh/id_rsa.pub)" 
+-p 2222:2222 
+$IMAGE_BU  
+DOCKER_RUN_END
+)
 
-	                docker_cmd=" docker run -d \
-									--name=$CONT_BU \
-									$src_dirs \
-									-e PUID=1001 \
-									-e PGID=1001 \
-									-e PASSWORD_ACCESS=true \
-									-e USER_PASSWORD=boo \
-									-e USER_NAME=${bu_username}\
-									-e PUBLIC_KEY=\"$(cat ~/.ssh/id_rsa.pub)\" \
-									-p 2222:2222 \
-									$IMAGE_BU "
-									echo "$docker_cmd"
-                  ssh ${REMOTE_HOST}  "$docker_cmd"
-
-#					while ! check_container_is_on; do
-#									sleep 1
-#					done
-          sleep 5
-					while ! ssh -vv -p2222 -J ${REMOTE_HOST} ${SSH_LOCAL_CONF} time ; do
-									echo "no connection"
-									sleep 1
-					done
-					echo "R E S U M E"
+                  ! { ssh ${REMOTE_HOST} docker ps | grep $CONT_BU -q; } \
+									&& ssh ${REMOTE_HOST}  "$docker_cmd" \
+									&& echo "created bu container $CONT_BU" \
+									&& echo re-run backup script \
+									&& return
+#For unknown reason ssh command run after container creation by "docker run"
+# usually fails even if delayed for 3-5 sec.
 
 					for src_dir in $(echo $src_dirs|sed 's/-v/\n/g'|sed '/^ *$/d'|sed 's/.*://'); do
-									echo "!!!>${src_dir}<"
+#									echo "!!!>${src_dir}<"
 
-rsync --rsh="ssh -p2222 -J ${REMOTE_HOST} ${SSH_LOCAL_CONF}" -avzH --delete "${bu_username}@localhost:${src_dir}" "${bu_local_dir}/" 
-#rsync_cmd=(	rsync --rsh="ssh -p2222 -J ${REMOTE_HOST} -o UserKnownHostsFile=/dev/null  -o StrictHostKeyChecking=no" -avzH -vvv --delete "${bu_username}@localhost:${src_dir}"  "${bu_local_dir}/" )
-#         printf '%q\n' "${rsync_cmd[@]}"
+                  rsync --rsh="ssh -p2222 -J ${REMOTE_HOST} ${SSH_LOCAL_CONF}" \
+													-avzH --delete "${bu_username}@localhost:${src_dir}"  \
+													"${bu_local_dir}/" 
 					done
 }
 
+
+function rsync-restore {
+     echo 'boo'
+}
+
 ########################################################################################
-#check if docker image for backup container exists on remote host
-log  0 "start job for remote host $REMOTE_HOST"
 
 
-for bu_cont in $(jq -r '.containers|keys[]' "$CONF_FL"); do
+function main {
+  for bu_cont in $(jq -r '.containers|keys[]' "$CONF_FL"); do
+    #stop container if required
+		local start_container=0
+		jq -er ".containers[\"${bu_cont}\"].conf?|select(.stop_before_bu==true)" "$CONF_FL">/dev/null \
+						&& echo "STOP CONTAINER $bu_cont" && docker stop $bu_cont && start_container=1
+
 		for task_k in $(jq -r ".containers[\"${bu_cont}\"].tasks|keys[]" "$CONF_FL"); do
-			#echo "2nd"
 			task=$(jq -r ".containers[\"${bu_cont}\"].tasks[$task_k]" "$CONF_FL")
-			case $(echo "${task}" | jq -r ".driver") in
-				"mysqldump")
-								sql-bu "$bu_cont" "$task"
+			echo "$task" | jq -e '.skip?==true' >/dev/null && continue
+
+			task_f="$(echo "$task" | jq -r ".driver")-$bu_mode"
+
+			case $task_f in
+				"mysqldump-bu")
+								mysqldump-bu "$bu_cont" "$task"
 								;;
-				"rsync")
+				 mysqldump-restore)
+								 mysqldump-restore "$bu_cont" "$task"
+								 ;;
+				"rsync-bu")
 								rsync-bu "$bu_cont" "$task"
+								;;
+				 rsync-restore)
+								rsync-restore "$bu_cont" "$task"
 								;;
 			esac
 		done
-done
+		[[ start_container -eq 1 ]] && docker start $bu_cont && start_container=0
+  done
+}
+
+log  0 "start job for remote host $REMOTE_HOST"
+main
